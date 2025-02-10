d1a60a7da214b4b544b3558b540396b4d725eb05
elchalom <elchalom@mcmaster.ca>
2025-02-10 10:14:23 -0500

    Changes to interpolate curve to take timestep as a parameter

--- thrustCurve.py
+++ thrustCurve.py
@@ -4,13 +4,15 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 def extractThrustcurve(filepath : str) -> pd.DataFrame:
+    
     # Read the CSV, skipping the first 4 rows (adjust if necessary)
     df = pd.read_csv(filepath, skiprows=4, header=0)
+    
     # Display the array (optional)
-    print(df)
+    # print(df)
     return df
 
-def interpolateCurve(df : pd.DataFrame) -> pd.DataFrame:
+def interpolateCurve(df : pd.DataFrame, timestep : float = 1e-4) -> pd.DataFrame:
     # Ensure Time (s) is numeric for interpolation
     
     timeVals = df['Time (s)'].to_numpy()       # Convert time column to numpy array
@@ -22,7 +24,8 @@ def interpolateCurve(df : pd.DataFrame) -> pd.DataFrame:
     
     interp_func = interp1d(df['Time (s)'], df['Thrust (N)'], kind='linear', fill_value='extrapolate')
     
-    newTime = np.arange(timeVals.min(), timeVals.max(), 0.0001)
+    # Rearrange arrays with necessary timestep
+    newTime = np.arange(timeVals.min(), timeVals.max(), timestep)
     newThrust = interp_func(newTime)
 
     # Create the new DataFrame
@@ -34,27 +37,11 @@ def updateCSV(df : pd.DataFrame, filename : str = 'ThrustCurve.csv') -> None:
     df.to_csv(filename, index=False)
     return
 
-def interpolateCurve_Pandas(df : pd.DataFrame) -> pd.DataFrame:
-    
-    # Ensure the time column is a datetime or numeric type
-    df['Time (s)'] = pd.to_datetime(df['Time (s)'])
-
-    # Set the time column as the index
-    df.set_index('Time (s)', inplace=True)
-
-    # Resample to 1ms intervals
-    df_resampled = df.resample('1ms').mean()
-
-    # Interpolate missing values linearly
-    df_interpolated = df_resampled.interpolate(method='linear')
-
-    return df_interpolated
-
-# extractThrustcurve("Cesaroni_10367N1800-P.csv")
-
 def main() -> None:
     motorFile = "Cesaroni_10367N1800-P.csv"
     df = extractThrustcurve(motorFile)
+    
+    ### TEST CSV TO CHECK INTERMEDIARY RESULTS ###
     # updateCSV(df, "Test.csv")
 
     interpolatedDataFrame = interpolateCurve(df)
